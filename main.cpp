a4d665a3098ffa0ed39eb686961095ec9cdaf10a
NahoYokoyama <na.74273.777@gmail.com>
2024-03-22 14:01:15 +0900

    fixed errors in input function

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-class main{
-    int starti;
-    int startj;
-    int[][] tileMap;
-    int[][] scoreMap;
+class Solver{
+    public: 
+        int starti;
+        int startj;
+        int tileMap[50][50];
+        int scoreMap[50][50];
 
-    int main(){
-        Start();
-        return 0;
-    }
+        void Start(){
+            GetInput();
+            DFS();
+        }
 
-    void Start(){
-        GetInput();
-    }
-
-    void GetInput(){
-        cin >> starti >> startj;
-        for (int i=0; i<50; i++){
-            for (int j=0; j<50; j++){
-                cin >> tileMap[i][j];
+        void GetInput(){
+            cin >> starti >> startj;
+            for (int i=0; i<50; i++){
+                for (int j=0; j<50; j++){
+                    cin >> tileMap[i][j];
+                }
+            }
+            for (int i=0; i<50; i++){
+                for (int j=0; j<50; j++){
+                    cin >> scoreMap[i][j];
+                }
             }
         }
-        for (int i=0; i<50; i++){
-            for (int j=0; j<50; j++){
-                cin >> scoreMap[i][j];
-            }
-        }
-    }
+};
+
+
+
+int main(){
+    Solver solver;
+    solver.Start();
+    return 0;
 }
