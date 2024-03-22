9fe22e393066669c2b500b1c310771644889ee97
史門 <s@MacBook-Air.local>
2024-03-22 13:47:21 +0900

    init

---
+++ ahc002_dfs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long ll;
+using namespace atcoder;
+using namespace std;
+int fld[50][50];
+int tiles[50][50];
+
+int main(){
+    int si, sj;
+    cin >> si >> sj;
+    
+
+    return 0;
+}
